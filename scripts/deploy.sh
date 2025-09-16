ac964f203f8d4f75814a439491bbc3218c62cee7
NamJihyun99 <namjihyun9936@gmail.com>
2025-09-16 14:37:10 +0900

    fix: 다시 올리기

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,15 +11,18 @@ if [ -z "$SERVICE_NAME" ]; then
    exit 1
 fi
 
-if command -v docker &> /dev/null; then
-   echo "Pulling latest image for $SERVICE_NAME..."
-   sudo docker-compose pull $SERVICE_NAME
-fi
+# if command -v docker &> /dev/null; then
+#    echo "Pulling latest image for $SERVICE_NAME..."
+#    sudo docker-compose pull $SERVICE_NAME
+# fi
+
+sudo docker-compose pull
+sudo docker-compose up -d --no-deps
 
 echo "Restarting $SERVICE_NAME service..."
 # sudo docker-compose stop $SERVICE_NAME
 # sudo docker-compose rm -f $SERVICE_NAME
-sudo docker-compose up -d $SERVICE_NAME
+# sudo docker-compose up -d $SERVICE_NAME
 
 # if [ "$SERVICE_NAME" == "api-server" ] || [ "$SERVICE_NAME" == "data-server" ]; then
 #    echo "Restarting nginx to apply changes..."
